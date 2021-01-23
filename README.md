067cbd8f14b27d6a38ee34306bc5ba0b7954e324
Kato Twofold <admin@justkato.me>
2021-01-23 11:14:46 +0200

    Starting

---
+++ README.md
@@ -0,0 +1,1 @@
+# [Kato's CLI Logger](https://github.com/JustKato/kclog) &middot; [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/JustKato/kclog/blob/master/LICENSE)
--- logger.js
+++ logger.js
@@ -1,3 +1,30 @@
+
+
 module.exports = {
-    
+
+    /**
+     * A quick access list to basic terminal colors
+     */
+    colors: {
+        Black: `\u001b[30m`,
+        Red: `\u001b[31m`,
+        Green: `\u001b[32m`,
+        Yellow: `\u001b[33m`,
+        Blue: `\u001b[34m`,
+        Magenta: `\u001b[35m`,
+        Cyan: `\u001b[36m`,
+        White: `\u001b[37m`,
+        Reset: `\u001b[0m`,
+    },
+
+}
+
+/**
+ * 
+ * @param {String} message The message to print out in the CLI
+ * @param {String} type The type of message to send
+ * @param {Object} options Extra options for the logging
+ */
+function log(message = ``, type = 'info', options = {}) {
+
 }
